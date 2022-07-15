94f0a063dc46c194cb50616718c2f3332abb557e
Dawn Marie <57174907+dawnmarieee@users.noreply.github.com>
2022-07-16 04:41:56 +0800

    Add files via upload
    responsive web design

---
+++ index.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="cache-control" content="no-cache">
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="HandheldFriendly" content="true">
+        <title>Welcome to AC-DC Service Center</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link href="./css/style6.css" rel="stylesheet" type="text/css">
+       
+    </head>
+    <body>
+        
+        <div class="grid">
+            <section id="floating"></section>
+            <header>
+                <nav>
+                    <div class="dropdown">
+                        <button class="dropbtn" id="hidebtn"><span class="fa fa-bars fa-2x" aria-hidden="true"></span></button>
+                            <div class="dropdown-content">
+                            <ul>
+                                <li><a class="#" href="#">Home</a></li>
+                                <li><a href="#">Explore</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#" target="_blank">Sign In</a></li>
+                            </ul>
+                            </div>
+                    </div>
+                </nav>
+                <div id="title01"><h2>AC-DC Service Center</h2></div>
+                <main class="contents box">
+                <div id="info1">
+                    <img class="graphic3" src="./images/tv.png"> 
+                    <img class="graphic2" src="./images/ref.png">
+                    <img class="graphic1" src="./images/tv.png">
+                    <img class="graphic0" src="./images/aircon.png">
+                </div>
+
+                <div class="info2">
+                        <h1>Aircon installation, maintenance & home repair services</h1>
+                        <p>Offers aircon general cleaning, repair, dismantling for window types and split types.</p><br>
+                        <a href="#" data-toggle="modal"><button class="button1" type="button">Book a Service</button><br></a> 
+                </div>
+                </main> 
+                
+                <footer>
+                    <p>️&copy;  2020</p>
+                </footer>
+                
+            </header>
+        </div>  
+            <!--jquery-->
+            <script type="text/javascript" src="js/jquery.js"></script>
+            
+            <script>
+                const faBar {
+
+                }
+            </script>
+    </body>
+</html>
